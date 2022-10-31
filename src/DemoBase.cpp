fb4690d27bfff01c5c594981a04c090baaf68fe1
Flávio Luiz Puhl Junior <flavio_puhl@hotmail.com>
2022-10-31 16:46:00 -0300

    Added connection to wifi and wifi verification

--- src/DemoBase.cpp
+++ src/DemoBase.cpp
@@ -1,11 +1,96 @@
+
+
+/*+--------------------------------------------------------------------------------------+
+ *| Libraries                                                                            |
+ *+--------------------------------------------------------------------------------------+ */
+
 #include <Arduino.h>
 
-// teste
+#ifdef ESP8266
+  #include <ESP8266WiFi.h>
+  extern "C" {
+  #include "user_interface.h"
+  } 
+#else
+  #include <WiFi.h>
+#endif
 
-void setup() {
-  // put your setup code here, to run once:
+
+/*+--------------------------------------------------------------------------------------+
+ *| Constants declaration                                                                |
+ *+--------------------------------------------------------------------------------------+ */
+ 
+const char *ssid                              = "CasaDoTheodoro1";                        // name of your WiFi network
+const char *password                          = "09012011";                               // password of the WiFi network
+
+
+/*+--------------------------------------------------------------------------------------+
+ *| Connect to WiFi network                                                              |
+ *+--------------------------------------------------------------------------------------+ */
+
+void setup_wifi() {
+  Serial.print("\nConnecting to ");
+  Serial.println(ssid);
+    WiFi.mode(WIFI_STA);                              // Setup ESP in client mode
+    //WiFi.setSleepMode(WIFI_NONE_SLEEP);
+    WiFi.begin(ssid, password);                       // Connect to network
+
+    int wait_passes = 0;
+    while (WiFi.status() != WL_CONNECTED) {           // Wait for connection
+      delay(500);
+      Serial.print(".");
+      if (++wait_passes >= 20) { ESP.restart(); }     // Restart in case of no wifi connection   
+    }
+
+  Serial.print("\nWiFi connected");
+  Serial.print("\nIP address: ");
+    Serial.println(WiFi.localIP());
+
+  WiFi.setAutoReconnect(true);
+  WiFi.persistent(true);
+
 }
 
+/*+--------------------------------------------------------------------------------------+
+ *| Verify and Manage WiFi network                                                       |
+ *+--------------------------------------------------------------------------------------+ */
+
+void VerifyWifi() {
+
+  if (WiFi.status() != WL_CONNECTED || WiFi.localIP() == IPAddress(0, 0, 0, 0)){          // Check for network health
+      
+    Serial.printf("error: WiFi not connected, reconnecting \n");
+            
+      WiFi.disconnect();
+      setup_wifi();             
+
+  } 
+
+}
+
+
+/*+--------------------------------------------------------------------------------------+
+ *| Setup                                                                                |
+ *+--------------------------------------------------------------------------------------+ */
+ 
+void setup() {
+
+  Serial.begin(115200);                                                                     // Start serial communication at 115200 baud
+  delay(1000);
+
+  setup_wifi(); 
+
+}
+
+
+
+/*+--------------------------------------------------------------------------------------+
+ *| main loop                                                                            |
+ *+--------------------------------------------------------------------------------------+ */
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  delay(1000);
+  VerifyWifi();   
+
 }
