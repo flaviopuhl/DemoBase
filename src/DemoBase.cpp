2e296198d50acd8aab4fe51eed6675109e124cc2
Flávio Luiz Puhl Junior <flavio_puhl@hotmail.com>
2022-11-01 15:18:59 -0300

    Added wakeup reason

--- src/DemoBase.cpp
+++ src/DemoBase.cpp
@@ -24,6 +24,8 @@
 
 #include <esp_task_wdt.h>                // Watchdog library
 
+#include <rom/rtc.h>
+
 /*+--------------------------------------------------------------------------------------+
  *| Constants declaration                                                                |
  *+--------------------------------------------------------------------------------------+ */
@@ -34,7 +36,7 @@ const char *password                          = "09012011";                     
 #define WATCHDOGTIMEOUT                       10
 
 unsigned int previousMillis = 0;
-
+int i=0;
 /*+--------------------------------------------------------------------------------------+
  *| Callback methods prototypes                                                          |
  *+--------------------------------------------------------------------------------------+ */
@@ -145,6 +147,33 @@ String DateAndTimeFormattedRTC(){
 }
 
 
+/*+--------------------------------------------------------------------------------------+
+ *| Method to print the reason by which ESP32 has been awaken from sleep                 |
+ *+--------------------------------------------------------------------------------------+ */
+
+void print_reset_reason(RESET_REASON reason)
+{
+  switch ( reason)
+  {
+    case 1 : Serial.println ("POWERON_RESET");break;          /**<1, Vbat power on reset*/
+    case 3 : Serial.println ("SW_RESET");break;               /**<3, Software reset digital core*/
+    case 4 : Serial.println ("OWDT_RESET");break;             /**<4, Legacy watch dog reset digital core*/
+    case 5 : Serial.println ("DEEPSLEEP_RESET");break;        /**<5, Deep Sleep reset digital core*/
+    case 6 : Serial.println ("SDIO_RESET");break;             /**<6, Reset by SLC module, reset digital core*/
+    case 7 : Serial.println ("TG0WDT_SYS_RESET");break;       /**<7, Timer Group0 Watch dog reset digital core*/
+    case 8 : Serial.println ("TG1WDT_SYS_RESET");break;       /**<8, Timer Group1 Watch dog reset digital core*/
+    case 9 : Serial.println ("RTCWDT_SYS_RESET");break;       /**<9, RTC Watch dog Reset digital core*/
+    case 10 : Serial.println ("INTRUSION_RESET");break;       /**<10, Instrusion tested to reset CPU*/
+    case 11 : Serial.println ("TGWDT_CPU_RESET");break;       /**<11, Time Group reset CPU*/
+    case 12 : Serial.println ("SW_CPU_RESET");break;          /**<12, Software reset CPU*/
+    case 13 : Serial.println ("RTCWDT_CPU_RESET");break;      /**<13, RTC Watch dog Reset CPU*/
+    case 14 : Serial.println ("EXT_CPU_RESET");break;         /**<14, for APP CPU, reseted by PRO CPU*/
+    case 15 : Serial.println ("RTCWDT_BROWN_OUT_RESET");break;/**<15, Reset when the vdd voltage is not stable*/
+    case 16 : Serial.println ("RTCWDT_RTC_RESET");break;      /**<16, RTC Watch dog reset digital core and rtc module*/
+    default : Serial.println ("NO_MEAN");
+  }
+}
+
 
 /*+--------------------------------------------------------------------------------------+
  *| Setup                                                                                |
@@ -184,6 +213,11 @@ void setup() {
   Serial.print("RTC formatted  : ");          // debug only
   Serial.println(DateAndTimeFormattedRTC());  // debug only
  
+  Serial.print("CPU0 rst reason: ");
+  print_reset_reason(rtc_get_reset_reason(0));
+
+  Serial.print("CPU1 rst reason: ");
+  print_reset_reason(rtc_get_reset_reason(1));
  
 
   Serial.print("\n\nSetup Finished\n\n");
@@ -210,9 +244,16 @@ void loop() {
 
   if (millis() - previousMillis >= 1000) {                                                  
      
+    //i++;
 
+    //if(i>=10){
+    //  Serial.println("Teste wtd");
+    //  delay(15000);
+    //}
+    
+    
     Serial.print("RTC formatted  : ");
-  Serial.println(DateAndTimeFormattedRTC());
+    Serial.println(DateAndTimeFormattedRTC());
 
     previousMillis = millis();
   }
